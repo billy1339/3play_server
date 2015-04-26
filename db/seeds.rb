3cfbe8810ce693f82d38a5e9b4c239cc71f15723
Billy1339 <william.m.sargent@gmail.com>
2015-04-26 17:28:46 -0400

    updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,11 @@ actor2 = Actor.create({name: "Georgous Women", url: "https://s-media-cache-ak0.p
 actor3 = Actor.create({name: "Stunning Women", url: "https://c1.staticflickr.com/3/2604/3859541980_53450c66f0.jpg", movie_id: movie1.id})
 actor4 = Actor.create({name: "Magnificient Women", url: "https://s-media-cache-ak0.pinimg.com/236x/d1/22/e5/d122e560fad8b100664dc355501f5bed.jpg", movie_id: movie1.id})
 actor5 = Actor.create({name: "Voluptious Women", url: "https://s-media-cache-ak0.pinimg.com/236x/2d/cb/2e/2dcb2efca09fe13023f245204b50cb60.jpg", movie_id: movie1.id})
+
+
+movie2 = Movie.create({title: "Super Cool Movie"})
+actor1 = Actor.create({name: "Person1", url: "http://www.alexwinn.com/wp-content/uploads/wpcu3er/8836/CU3ER_headshots_splashpage_v2/images/Emily-Laing.jpg", movie_id: movie2.id})
+actor2 = Actor.create({name: "Person2", url: "https://s-media-cache-ak0.pinimg.com/236x/b0/15/b6/b015b6aa3111f18ece4be3fd99249992.jpg", movie_id: movie2.id})
+actor3 = Actor.create({name: "Person3", url: "https://c1.staticflickr.com/3/2604/3859541980_53450c66f0.jpg", movie_id: movie2.id})
+actor4 = Actor.create({name: "Person4", url: "https://s-media-cache-ak0.pinimg.com/236x/d1/22/e5/d122e560fad8b100664dc355501f5bed.jpg", movie_id: movie2.id})
+actor5 = Actor.create({name: "Person5", url: "https://s-media-cache-ak0.pinimg.com/236x/2d/cb/2e/2dcb2efca09fe13023f245204b50cb60.jpg", movie_id: movie2.id})
